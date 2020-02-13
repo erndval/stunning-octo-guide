f91b0e26c81ddb2b8125e815126edaf7ebeaf7f8
github username <erndval@gmail.com>
2020-02-12 19:41:01 -0500

    Conditional rendering

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -11,16 +11,23 @@ class Counter extends Component {
     fontWeight: "bold"
   };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.renderTags()}
       </div>
     );
   }
