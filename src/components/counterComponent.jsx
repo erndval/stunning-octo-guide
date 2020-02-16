e70dbcb0f885c65c8a5ff10af1f818b619eb4f96
github username <erndval@gmail.com>
2020-02-16 18:34:13 -0500

    Updating the State

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -22,15 +22,18 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement() {
-    console.log("Increment Clicked", this);
-  }
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={} className="btn btn-secondary btn-sm">
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
           Increment
         </button>
         {this.renderTags()}
